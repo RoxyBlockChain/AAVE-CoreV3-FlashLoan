fd0602404f8661ccc018f55c0a04186fc2ea1e13
Yasir Iqbal <105126069+RoxyBlockChain@users.noreply.github.com>
2023-04-15 17:08:18 +0500

    TestNet Speolia Used

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.10",
   networks:{
-    goerli:{
+    sepoila:{
       url: process.env.INFURA_API_ENDPOINT,
       accounts: [process.env.PRIVATE_KEY],
     },    
--- scripts/deployFlashLoan.js
+++ scripts/deployFlashLoan.js
@@ -4,13 +4,13 @@ async function main() {
   
 const FlashLoan = await hre.ethers.getContractAt("FlashLoan");
 const flashLoan = await FlashLoan.deploy(
-  "0x5E52dEc931FFb32f609681B8438A51c675cc232d"
-);  
+  "0x0496275d34753A48320CA58103d5220d394FF77F"
+);
 // during deployent of the contract, need to pass the Arguments of Constractor
 
 await flashLoan.deployed();
 
-console.log(" Aave Flash Loan contract Deployed on Address:", flashLoan.address)
+console.log("Aave Flash Loan contract Deployed on Address:", flashLoan.address)
   
 }
 
